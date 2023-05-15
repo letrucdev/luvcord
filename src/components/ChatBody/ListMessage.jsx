005722550646b7c38e95382b25154b5e96970af6
Trực Lê <lechinhtruc999@gmail.com>
2023-05-15 16:51:45 +0700

    fix bug UI

--- src/components/ChatBody/ListMessage.jsx
+++ src/components/ChatBody/ListMessage.jsx
@@ -10,8 +10,8 @@ export default function ListMessage({ children }) {
   }, []);
 
   return (
-    <div className="flex min-h-0 h-full">
-      <div className="overflow-y-scroll">
+    <div className="flex min-h-0 w-full h-full">
+      <div className="overflow-y-scroll w-full">
         <div className="flex flex-col justify-end min-h-full ">
           <ol className="overflow-hidden">
             
